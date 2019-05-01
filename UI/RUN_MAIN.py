f6f7faeb44c9d4af49d97192cfc18d5f8f0cea74
jgyyz <1624180634@qq.com>
2019-05-01 13:31:48 +0800

    不同品牌好评/差评数据表格展示

--- UI/RUN_MAIN.py
+++ UI/RUN_MAIN.py
@@ -8,65 +8,77 @@
 @time: 2019/4/30 13:35
 @desc:
 '''
-from PyQt5.QtWidgets import QApplication, QWidget, QMessageBox, QHeaderView, QPushButton, QLabel, QHBoxLayout
-import sys
-import numpy as np
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
 from main import Ui_Form
+import sys
+import pymysql
+
+_brand = ['hpscorepos', 'hpscoreneg','lenovoscorepos', 'lenovoscoreneg']
 
 class mywindow(Ui_Form, QWidget):
+
     def __init__(self, parent=None):
         super(mywindow, self).__init__(parent)
         self.setupUi(self)
         self.setWindowTitle('笔记本电脑评论情感分析系统')
-        self.table()
+        self.tableComments(_brand[0])    # 默认初始化为惠普好评数据
+        self.comboBox.currentIndexChanged.connect(self.comboBoxSelected)
 
-        self.layout = QHBoxLayout()
-        self.setLayout(self.layout)
-        self.layout.addStretch()
-        self.btn_left = QPushButton('<<')
-        self.btn_left.setStyleSheet("QPushButton{border:0;font-size:16px}QPushButton::hover{background-color:green}")
-        self.layout.addWidget(self.btn_left)
-        self.label_page = QLabel('1')
-        self.label_page.setStyleSheet("font-size:14px;")
-        self.layout.addWidget(self.label_page)
-        self.btn_right = QPushButton('>>')
-        self.btn_right.setStyleSheet("QPushButton{border:0;font-size:16px}QPushButton::hover{background-color:green}")
-        self.layout.addWidget(self.btn_right)
-        self.layout.addStretch()
-        self.label_all = QLabel('共1页')
-        self.label_all.setStyleSheet("font-size:14px;")
-        self.layout.addWidget(self.label_all)
+    def tableComments(self, tableName):
+        data = self.dbConnect(tableName)
+        row = len(data)
+        vol = len(data[1])
 
-        self.current_page = 1
-        self.all_page = 1
-        self.lenth = 1
-        self.num = 1
+        # 设置数据层次结构，10行2列
+        self.model = QStandardItemModel(10, 2)
+        # 设置水平方向四个头标签文本内容
+        self.model.setHorizontalHeaderLabels(['序号', '评论'])
+        for i in range(row):
+            for j in range(vol):
+                temp_data = data[i][j]  # 临时记录，不能直接插入表格
+                # print(temp_data)
+                item = QStandardItem(str(temp_data))
+                self.model.setItem(i, j, item)
 
-    def table(self):
-        # 列宽根据内容自适应
-        self.tableWidget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
-        self.tableWidget.horizontalHeader().setSectionResizeMode(0, QHeaderView.Interactive)
+        self.tableView.setModel(self.model)
+        # 设置行样式
+        self.tableView.setAlternatingRowColors(True)
+        # 设置不同列宽
+        self.tableView.setColumnWidth(0, 45)
+        self.tableView.setColumnWidth(1, 800)
+        # 隐藏横向滚动条
+        self.tableView.setHorizontalScrollBarPolicy(True)
+        layout = QVBoxLayout()
+        # 将下拉框与表格放进同一个layout
+        layout.addWidget(self.comboBox)
+        layout.addWidget(self.tableView)
+        layout.setSpacing(10)
+        # layout.setStretchFactor(self.comboBox, 1)
+        # layout.setStretchFactor(self.tableView, 10)
+        self.tabComments.setLayout(layout)
+        # 所有列自动拉伸，充满界面，适用于列数较多且内容合适的表格model
+        # self.tableView.horizontalHeader().setStretchLastSection(True)
+        # self.tableView.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
 
-    def set_page_num(self, lenth, num):
-        self.lenth = lenth
-        self.num = num
-        if lenth % num == 0:
-            self.all_page = lenth // num
-        else:
-            self.all_page = lenth // num + 1
-        self.refresh_page_info()
+    def comboBoxSelected(self, index):
+        print('当前项：', self.comboBox.itemText(index))
+        self.tableComments(_brand[index])
 
-    def refresh_page_info(self):
-        self.label_all.setText('共' + str(self.all_page) + '页')
-        self.label_page.setText(str(self.current_page))
-
-    def get_index(self, index):
-        if index < self.all_page:
-            return self.num * (index - 1), self.num * index - 1
-        elif index == self.all_page:
-            return self.num * (index - 1), self.lenth - 1
-        else:
-            return 0, 0
+    # 连接数据库
+    def dbConnect(self, tableName):
+        try:
+            db = pymysql.connect("localhost", "root", "123456", "preprocessing", charset='utf8')
+            cur = db.cursor()
+            cur.execute("SELECT * FROM "+tableName+" WHERE id BETWEEN 1 AND 100")
+            data = cur.fetchall()
+            print('SUCCESS: 数据库连接成功！')
+            cur.close()
+            db.close()
+            return data
+        except:
+            print('ERROR: 数据库连接失败！')
+            return None
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
